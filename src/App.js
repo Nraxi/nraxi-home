511319cba63644e70e3e5e7c4960940cd408a1c0
nraxi <olle.code@hotmail.com>
2023-11-15 21:00:39 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 function App() {
+
+  const backgroundStyle = {
+    backgroundImage: `url(${process.env.PUBLIC_URL}/gamingbg.png)`
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="background-container" style={backgroundStyle}>
+      <div className='lustigalöken'>
+        <h1>Welcome to My App</h1>
+        <p>hey</p>
+      </div>
     </div>
   );
 }
