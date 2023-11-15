6bec056a04637b4b239e485ef1f20eca01b329ca
nraxi <olle.code@hotmail.com>
2023-11-16 00:01:15 +0100

    small changes

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -6,18 +6,16 @@ function Navbar() {
     <div>
       <header id="header">
         <div class="container">
-          <h1><a className="namnet" href="#">Olle Tengnér</a></h1>
-          <h2>I'm a passionate <span>graphic designer</span> from Stockholm, Sweden</h2>
-          <div class="social-links">
-            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
-            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
-            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
-            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
-          </div>
+          <h2 className="hej">Hejsan och Välkommen</h2>
+          <h2 className="passion">I'm a passionate: <span className='passionSpan'>graphic designer</span>
+          </h2>
+
+          <h2 className="fransthlm">From Stockholm, Sweden</h2>
+
 
           <nav id="navbar" class="navbar">
             <ul>
-              <li><a class="nav-link active" href="#header">Home</a></li>
+              <li><a class="nav-link" href="#">Home</a></li>
               <li><a class="nav-link" href="#about">About</a></li>
               <li><a class="nav-link" href="#resume">Resume</a></li>
               <li><a class="nav-link" href="#services">Services</a></li>
@@ -29,8 +27,16 @@ function Navbar() {
 
 
         </div>
-      </header>
-    </div>
+      </header >
+      <div class="social-links">
+        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+      </div>
+      <h1 className="namnet"><a className="namnetfor" href="#">Name: Olle </a><a className="namnetefter" href="#"> Tengnér</a></h1>
+
+    </div >
   )
 }
 
