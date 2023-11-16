87d1548b3fc999f5747ac32e220a0712c3d14113
nraxi <olle.code@hotmail.com>
2023-11-16 03:37:16 +0100

    ok

---
+++ src/components/Pages/AboutMe.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AboutMe() {
+  return (
+    <div><h1>ABOUT ME HEY</h1></div>
+  )
+}
+
+export default AboutMe
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
+import * as ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './css/index.css';
 import App from './App';
+import ErrorPage from './components/Pages/ErrorPage';
+import Home from './components/Pages/Home';
+import AboutMe from './components/Pages/AboutMe';
 
 
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/About",
+        element: <AboutMe />,
+      }
+
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <App />
-  </Router>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 );
-
