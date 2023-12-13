fec9a1e1b6d941220faf638bab09d24651efdb32
nraxi <olle.code@hotmail.com>
2023-12-13 11:18:53 +0100

    small

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ import Gaming from './components/Pages/Gaming';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/nraxi-home/",
     element: <App />,
     errorElement: <ErrorPage />,
   },
