56cc446c2c6a16905ebede0dc127bb4b820bc531
nraxi <olle.code@hotmail.com>
2024-01-02 18:28:53 +0100

    changeup css and hitup the links

--- src/components/carousel/Karusell.jsx
+++ src/components/carousel/Karusell.jsx
@@ -4,18 +4,18 @@ import "../../css/carousel-image.css";
 function Karusell() {
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const images = React.useMemo(() => [
-    require('../images/WebGallery/Screenshot-Homepage.png'),
-    require('../images/WebGallery/Screenshot-snowmedia.png'),
+  const slides = React.useMemo(() => [
+    { image: require('../images/WebGallery/Screenshot-Homepage.png'), link: 'https://nraxi.github.io/nraxi-home', text: 'Homepage' },
+    { image: require('../images/WebGallery/Screenshot-snowmedia.png'), link: 'https://nraxi.github.io/snowmedia/', text: 'Snow Media' },
   ], []);
 
   const nextSlide = useCallback(() => {
-    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-  }, [images]);
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % slides.length);
+  }, [slides]);
 
   const prevSlide = useCallback(() => {
-    setCurrentIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-  }, [images]);
+    setCurrentIndex((prevIndex) => (prevIndex - 1 + slides.length) % slides.length);
+  }, [slides]);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
@@ -31,9 +31,9 @@ function Karusell() {
     <div className="carousel1">
       <span className="bi bi-caret-right-fill carousel-control1" onClick={nextSlide}></span>
       <span className="bi bi-caret-left-fill carousel-control1" onClick={prevSlide}></span>
-      <img className="carousel-image1" src={images[currentIndex]} alt={`Slide ${currentIndex + 1}`} />
+      <img className="carousel-image1" src={slides[currentIndex].image} alt={`Slide ${currentIndex + 1}`} />
       <div className="carousel-indicators1">
-        {images.map((_, index) => (
+        {slides.map((_, index) => (
           <span
             key={index}
             className={`indicator1 ${index === currentIndex ? 'active1' : ''}`}
@@ -41,6 +41,12 @@ function Karusell() {
           ></span>
         ))}
       </div>
+      <p className="linkforsite">
+        <a href={slides[currentIndex].link} target="_blank" rel="noopener noreferrer">
+          {slides[currentIndex].text}
+        </a>
+      </p>
+
     </div>
   );
 }
